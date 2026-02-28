cf36debe9bc41afc960acec6dfde0087846dedfe
hossainraj2002 <akbarhossenraj@gmail.com>
2026-02-28 15:06:23 +0600

    feat: update next.config.js to enhance webpack configuration and add new image remote patterns

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,12 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'assets.coingecko.com' },
       { protocol: 'https', hostname: 'token-icons.s3.amazonaws.com' },
       { protocol: 'https', hostname: 'logos.covalenthq.com' },
     ],
+  },
+
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@react-native-async-storage/async-storage': false,
+      'react-native': false,
+    };
+    return config;
   },
 };
 
